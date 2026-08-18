2b3dd7c0888ae8cc021570fa3bda7445fc7b9aaa
nakulmtr25-afk <nakulmtr25@gmail.com>
2026-08-18 20:28:28 +0530

    Implement root calculation for quadratic equations
    Added calculation for roots of the quadratic equation and updated output messages.

--- findingroots.c
+++ findingroots.c
@@ -1,7 +1,7 @@
 // Q17: Write a program to find the roots of a quadratic equation and categorize them.
 #include<stdio.h>
 int main(){
-    double a,b,c,D;
+    double a,b,c,D,root1,root2,root;
     printf("Enter a : ");
     scanf("%lf",&a);
     printf("Enter b : ");
@@ -9,14 +9,17 @@ int main(){
     printf("Enter c : ");
     scanf("%lf",&c);
     D = (b*b) - (4*a*c);
+    root1 = (-b + sqrt(D))/2*a;
+    root2 = (-b - sqrt(D))/2*a;
+    root = b/(2*a);
     if(D>0){
-        printf(" Roots are real and different");
+        printf("%.1lf %.1lf Roots are real and different",root1,root2 );
     }
     else if(D<0){
         printf("Roots are imaginary  and complex");
     }
     else{
-        printf("Roots are real and same");
+        printf("%.1lf Roots are real and same",root);
     }
     return 0;
 }
